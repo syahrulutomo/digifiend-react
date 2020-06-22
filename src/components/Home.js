7d9ceb323c103b51a4595fa5a873f0ad8723fbde
syahrulutomo <irullvandalis@gmail.com>
2020-06-22 22:32:40 +0700

    remove unused code

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,6 @@ import PropTypes from 'prop-types';
 import Article from './Article';
 
 export class Home extends Component {
-    constructor(props){
-        super(props);
-    }
-
     componentDidMount(){
         this.props.onFetchNews(this.props.country);
     }
